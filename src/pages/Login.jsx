a0852b5193e9c87a9b36eeb6e191b70e8a5dc3a2
gauravsinhaweb <sinhagaurav.me@gmail.com>
2022-07-19 23:17:00 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,9 @@ import useAuth from "../custom-hook/useAuth";
 import { authActions } from "../redux/reducers/authSlice";
 import { loginHandler } from "../utils";
 import { useNavigate } from "react-router-dom";
+import firebaseIcon from "../assets/firebase.png";
+import reduxIcon from "../assets/redux.png";
+import tailwindIcon from "../assets/tailwind.png";
 
 export const Login = () => {
   const [loginWithGoogle] = useAuth();
@@ -15,8 +18,7 @@ export const Login = () => {
     <div className="h-88 text-white flex justify-center items-center">
       <div className="lg:w-1/3 h-1/2 p-8 bg-gray-900 rounded-xl">
         <h1 className="text-center text-2xl uppercase font-semibold">login</h1>
-        {/* sign in with google  */}
-        <div className="flex justify-center flex-col gap-8 items-center h-full">
+        <div className="flex relative pb-12 justify-center flex-col gap-8 items-center h-full">
           <button
             onClick={() =>
               loginHandler(loginWithGoogle, dispatch, authActions, navigate)
@@ -38,9 +40,38 @@ export const Login = () => {
               <FaTwitter size={20} />
             </span>
             <span> Sign in with Twitter</span>
-          </button>
+          </button>{" "}
+          <div className="absolute bottom-2 my-2">
+            <div className=" text-sm text-gray-400 my-2 space-mono text-center">
+              {" "}
+              Made with -
+            </div>
+            <div className="flex gap-4">
+              <img
+                title="Firebase"
+                src={firebaseIcon}
+                alt="firebase"
+                className="h-6 w-6"
+              />
+              <img
+                title="Redux-Toolkit"
+                src={reduxIcon}
+                alt="redux"
+                className="h-6 w-6"
+              />
+              <img
+                title="TailwindCSS"
+                src={tailwindIcon}
+                alt="tailwind"
+                className="h-6 w-6"
+              />
+            </div>
+          </div>
         </div>
       </div>{" "}
+      <div className="absolute bottom-2 text-gray-600 text-sm space-mono">
+        Developed by Gaurav Sinha • 2022
+      </div>
     </div>
   );
 };
